5a5189512e51f478f9fff1faf62b3166c0572a17
harshdeep2909 <90376919+harshdeep2909@users.noreply.github.com>
2022-02-02 18:23:05 +0530

    Add files via upload

---
+++ Tree/PostOrderTraversal.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+void inorder(struct Node* root){
+    if (root==NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+    
+    
+}
+void postorder(struct Node* root){
+    if (root==NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right); 
+    cout<<root->data<<" ";
+}
+int main(){
+    struct Node* root = new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+     root->left->right=new Node(5);
+    root->right->right=new Node(7);
+    root->right->left=new Node(6);
+    cout<<"PostOrder:"<<endl;
+    postorder(root);
+    return 0;
+}
